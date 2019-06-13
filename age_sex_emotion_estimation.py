299ccb415c4f2a6365f319f3deccd1d8286b4a16
shieshi <50486318+shieshi@users.noreply.github.com>
2019-06-13 11:23:45 +0400

    Create age_sex_emotion_estimation.py

---
+++ age_sex_emotion_estimation.py
@@ -0,0 +1,257 @@
+import http.client
+import mimetypes
+import os
+import requests
+import time
+from pathlib import Path
+import cv2
+from keras.models import load_model
+import dlib
+import numpy as np
+import argparse
+from contextlib import contextmanager
+from wide_resnet import WideResNet
+from keras.utils.data_utils import get_file
+
+pretrained_model = "https://github.com/yu4u/age-gender-estimation/releases/download/v0.5/weights.28-3.73.hdf5"
+modhash = 'fbe63257a054c1c5466cfd7bf14646d6'
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="This script detects faces from web cam input, "
+                                                 "and estimates age and gender for the detected faces.",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--weight_file", type=str, default=None,
+                        help="path to weight file (e.g. weights.28-3.73.hdf5)")
+    parser.add_argument("--depth", type=int, default=16,
+                        help="depth of network")
+    parser.add_argument("--width", type=int, default=8,
+                        help="width of network")
+    parser.add_argument("--margin", type=float, default=0.4,
+                        help="margin around detected face for age-gender estimation")
+    parser.add_argument("--image_dir", type=str, default=None,
+                        help="target image directory; if set, images in image_dir are used instead of webcam")
+    args = parser.parse_args()
+    return args
+
+
+def draw_label(image, point, label, font=cv2.FONT_HERSHEY_SIMPLEX,
+               font_scale=0.8, thickness=1):
+    size = cv2.getTextSize(label, font, font_scale, thickness)[0]
+    x, y = point
+    cv2.rectangle(image, (x, y - size[1]), (x + size[0], y), (255, 0, 0), cv2.FILLED)
+    cv2.putText(image, label, point, font, font_scale, (255, 255, 255), thickness, lineType=cv2.LINE_AA)
+
+
+@contextmanager
+def video_capture(*args, **kwargs):
+    cap = cv2.VideoCapture(*args, **kwargs)
+    try:
+        yield cap
+    finally:
+        cap.release()
+
+
+def yield_images():
+    # capture video
+    with video_capture(0) as cap:
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+        while True:
+            # get video frame
+            ret, img = cap.read()
+
+            if not ret:
+                raise RuntimeError("Failed to capture image")
+
+            yield img
+
+
+def yield_images_from_dir(image_dir):
+    image_dir = Path(image_dir)
+
+    for image_path in image_dir.glob("*.*"):
+        img = cv2.imread(str(image_path), 1)
+
+        if img is not None:
+            h, w, _ = img.shape
+            r = 640 / max(w, h)
+            yield cv2.resize(img, (int(w * r), int(h * r)))
+
+def make_face_coordinates(detected_face):
+    x = detected_face.left()
+    y = detected_face.top()
+    width = detected_face.right() - detected_face.left()
+    height = detected_face.bottom() - detected_face.top()
+    return [x, y, width, height]
+
+def apply_offsets(face_coordinates, offsets):
+    x, y, width, height = face_coordinates
+    x_off, y_off = offsets
+    return (x - x_off, x + width + x_off, y - y_off, y + height + y_off)
+
+def preprocess_input(x, v2=True):
+    x = x.astype('float32')
+    x = x / 255.0
+    if v2:
+        x = x - 0.5
+        x = x * 2.0
+    return x
+
+def get_labels(dataset_name):
+    if dataset_name == 'fer2013':
+        return {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy',
+                4: 'sad', 5: 'surprise', 6: 'neutral'}
+    elif dataset_name == 'imdb':
+        return {0: 'woman', 1: 'man'}
+    elif dataset_name == 'KDEF':
+        return {0: 'AN', 1: 'DI', 2: 'AF', 3: 'HA', 4: 'SA', 5: 'SU', 6: 'NE'}
+    else:
+        raise Exception('Invalid dataset name')
+
+def window_click(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONUP:
+        global upload
+
+        upload = True
+
+
+def post_multipart(host, selector, files):
+    content_type, body = encode_multipart_formdata(files)
+    h = http.client.HTTP(host)
+    h.putrequest('POST', selector)
+    h.putheader('content-type', content_type)
+    h.putheader('content-length', str(len(body)))
+    h.endheaders()
+    h.send(body)
+    errcode, errmsg, headers = h.getreply()
+    return h.file.read()
+
+def encode_multipart_formdata(files):
+    BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
+    CRLF = '\r\n'
+    L = []
+    for (key, filename, value) in files:
+        L.append('--' + BOUNDARY)
+        L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
+        L.append('Content-Type: %s' % get_content_type(filename))
+        L.append('')
+        L.append(value)
+    L.append('--' + BOUNDARY + '--')
+    L.append('')
+    body = CRLF.join(L)
+    content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
+    return content_type, body
+
+def get_content_type(filename):
+    return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
+
+def main():
+    global upload
+
+    upload = False
+
+    args = get_args()
+    depth = args.depth
+    k = args.width
+    weight_file = args.weight_file
+    margin = args.margin
+    image_dir = args.image_dir
+
+    if not weight_file:
+        weight_file = get_file("weights.28-3.73.hdf5", pretrained_model, cache_subdir="pretrained_models",
+                               file_hash=modhash, cache_dir=Path(__file__).resolve().parent)
+
+    # for face detection
+    detector = dlib.get_frontal_face_detector()
+
+    # load model and weights
+    img_size = 64
+    model = WideResNet(img_size, depth=depth, k=k)()
+    model.load_weights(weight_file)
+
+    emotion_model_path = './pretrained_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
+    emotion_classifier = load_model(emotion_model_path, compile=False)
+    emotion_target_size = emotion_classifier.input_shape[1:3]
+    emotion_window = []
+    emotion_offsets = (20, 40)
+    emotion_labels = get_labels('fer2013')
+
+    image_generator = yield_images_from_dir(image_dir) if image_dir else yield_images()
+    cv2.namedWindow("result",cv2.WINDOW_NORMAL)
+    cv2.setMouseCallback("result", window_click)
+    for img in image_generator:
+        input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img_h, img_w, _ = np.shape(input_img)
+
+        # detect faces using dlib detector
+        detected = detector(input_img, 1)
+        faces = np.empty((len(detected), img_size, img_size, 3))
+
+        if len(detected) > 0:
+            for i, d in enumerate(detected):
+                x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+                xw1 = max(int(x1 - margin * w), 0)
+                yw1 = max(int(y1 - margin * h), 0)
+                xw2 = min(int(x2 + margin * w), img_w - 1)
+                yw2 = min(int(y2 + margin * h), img_h - 1)
+                # cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
+                faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
+
+            # predict ages and genders of the detected faces
+            results = model.predict(faces)
+            predicted_genders = results[0]
+            ages = np.arange(0, 101).reshape(101, 1)
+            predicted_ages = results[1].dot(ages).flatten()
+
+            gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # draw results
+            for i, d in enumerate(detected):
+                face_coordinates = make_face_coordinates(d)
+                x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+                gray_face = gray_image[y1:y2, x1:x2]
+                try:
+                    gray_face = cv2.resize(gray_face, (emotion_target_size))
+                except:
+                    continue
+                gray_face = preprocess_input(gray_face, False)
+                gray_face = np.expand_dims(gray_face, 0)
+                gray_face = np.expand_dims(gray_face, -1)
+                emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+                emotion_text = emotion_labels[emotion_label_arg]
+
+                label = emotion_text + " {}, {}".format(int(predicted_ages[i]-4),
+                                        "F" if predicted_genders[i][0] > 0.5 else "M")
+
+                if upload:
+                    filename = str(int(time.time())) + str(i) + ".jpg"
+                    if cv2.imwrite(filename, img[y1:y2, x1:x2]):
+                        with open(filename, 'rb') as uploadfile:
+                            files = {"image": (filename, open(filename, 'rb'), 'multipart/form-data')}
+                            requests.post("http://localhost/pepper_services/public/emotions/post_emotion_image/" + emotion_text, files=files)
+
+                        # os.remove(filename)
+                    else:
+                        print("error saving image!")
+
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                draw_label(img, (d.left(), d.top()), label)
+
+            if upload:
+                upload = False
+                cv2.setWindowTitle("result", "uploaded!")
+
+        Bigimg = cv2.resize(img, (1920, 1080)) 
+        # cv2.namedWindow("result", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow("result", 1920,1080)
+        cv2.imshow("result", Bigimg)
+        key = cv2.waitKey(-1) if image_dir else cv2.waitKey(30)
+
+        if key == 27:  # ESC
+            break
+
+
+if __name__ == '__main__':
+    main()
